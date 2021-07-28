30ee746130197d955ca36bd88d3a1c81ae872ca0
viettran1010 <viettran10101988@gmail.com>
2021-07-28 22:08:11 +0700

    Block and genesis

---
+++ block.js
@@ -0,0 +1,25 @@
+// definition of a block in blockchain
+const {GENESIS_DATA} = require("./config")
+
+class Block {
+    constructor({timestamp, lastHash, hash, data}) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    static genesis() {
+        return new Block(GENESIS_DATA);
+    }
+
+    static mineBlock({lastBlock,data}) {
+        return new Block({
+            timestamp: Date.now(),
+            lastHash: lastBlock.hash,
+            data
+        });
+    }
+}
+
+module.exports = Block
