a87667ba161d25d8d53f15c6347aa38b53d49b8d
viettran1010 <viettran10101988@gmail.com>
2021-07-29 18:27:22 +0700

    api to mine

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
+const bodyParser = require('body-parser');
 
 const app = express();
 const blockChain = new Blockchain();
 
+app.use(bodyParser.json())
+
 app.get('/api/blocks', (req,res)=> {
     res.json(blockChain.chain)
+})
+
+app.post('/api/mine', (req,res)=> {
+    const {data} = req.body;
+
+    blockChain.addBlock({data});
+
+    res.redirect('/api/blocks');
 })
 
 const PORT = 3000;
