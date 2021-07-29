fea39ebaf0fb114001efc4375fcbbe7f3bae11e3
viettran1010 <viettran10101988@gmail.com>
2021-07-29 20:36:53 +0700

    generate port for new node instances

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ app.post('/api/mine', (req,res)=> {
     res.redirect('/api/blocks');
 })
 
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+let PEER_PORT ;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, ()=> {
     console.log(`listening at port ${PORT}`);
 })
