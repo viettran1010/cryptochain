10ab500d113d969383ff0bf3ed345bbbff36e9d0
viettran1010 <viettran10101988@gmail.com>
2021-07-30 10:51:18 +0700

    add wallet feature

---
+++ wallet/index.test.js
@@ -0,0 +1,41 @@
+const { it } = require('@jest/globals');
+const Wallet = require('./index');
+const {verifySignature} = require('../util')
+
+describe('Wallet', ()=>{
+    let wallet;
+
+    beforeEach(()=> {
+        wallet = new Wallet();
+    })
+
+    it('has a balance', ()=>{
+        expect(wallet).toHaveProperty('balance');
+    })
+
+    it('has a public key', ()=>{
+        expect(wallet).toHaveProperty('publicKey');
+    })
+
+    describe('signing data', ()=>{
+        const data = 'foobar';
+
+        it('verifies a signature', ()=>{
+            expect(            
+                verifySignature({ 
+                publicKey: wallet.publicKey, 
+                data,
+                signature: wallet.sign(data) //can only verify with the same wallet with public/private key
+            })).toBe(true)
+        })
+
+        it('does not verify an invalid signature', ()=>{
+            expect(            
+                verifySignature({ 
+                publicKey: wallet.publicKey, 
+                data,
+                signature: (new Wallet()).sign(data) //different wallet
+            })).toBe(false)
+        })
+    })
+})
