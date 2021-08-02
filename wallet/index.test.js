7d4e752174d93b6bf9d07935dfd4033fbda1cf92
viettran1010 <viettran10101988@gmail.com>
2021-08-02 18:02:34 +0700

    calculate balance

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -2,6 +2,8 @@ const { it } = require('@jest/globals');
 const Wallet = require('./index');
 const {verifySignature} = require('../util')
 const Transaction = require('./transaction')
+const BlockChain = require('../blockchain')
+const {STARTING_BALANCE} = require('../config')
 
 describe('Wallet', ()=>{
     let wallet;
@@ -67,6 +69,50 @@ describe('Wallet', ()=>{
 
             it('outputs the amount of the recipient', ()=> {
                 expect(transaction.outputMap[recipient]).toEqual(amount)
+            })
+        })
+    })
+
+    describe('calculateBalance()', ()=>{
+        let blockchain;
+
+        beforeEach(()=> {
+            blockchain = new BlockChain();
+            console.log(blockchain);
+        })
+
+        describe('and there are no ouput for the wallet', ()=>{
+            it('returns the STARTING_BALANCE', ()=> {
+                expect(Wallet.calculateBalance({
+                    chain: blockchain.chain,
+                    address: wallet.publicKey
+                })).toEqual(STARTING_BALANCE)
+            })
+        })
+
+        describe('and there are outputs for the wallet', ()=> {
+            let transactionOne, transactionTwo;
+
+            beforeEach(() => {
+                transactionOne = new Wallet().createTransaction({
+                    recipient: wallet.publicKey,
+                    amount: 50
+                })
+
+                transactionTwo = new Wallet().createTransaction({
+                    recipient: wallet.publicKey,
+                    amount: 60
+                })
+
+                blockchain.addBlock({data: [transactionOne,transactionTwo]})
+            })
+
+            it('adds the sum of all outputs to the wallet balance', ()=> {
+                expect(Wallet.calculateBalance({
+                    chain: blockchain.chain,
+                    address: wallet.publicKey
+                })).toEqual(STARTING_BALANCE+transactionOne.outputMap[wallet.publicKey]+
+                    transactionTwo.outputMap[wallet.publicKey])
             })
         })
     })
