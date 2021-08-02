42b9bb6f7387f8e4481bccf23a3f77c8a61756c3
viettran1010 <viettran10101988@gmail.com>
2021-08-02 10:27:10 +0700

    transaction pool feature

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const Blockchain = require('./blockchain');
 const request = require('request');
 const bodyParser = require('body-parser');
-const PubSub = require('./app/pubsub')
+const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({blockchain})
 
 const DEFAULT_PORT = 3000;
@@ -25,6 +29,29 @@ app.post('/api/mine', (req,res)=> {
     pubsub.broadcastChain();
 
     res.redirect('/api/blocks');
+})
+
+app.post('/api/transact', (req,res)=> {
+    const {amount,recipient} = req.body;
+
+    let transaction = transactionPool.existingTransaction({inputAddress: wallet.publicKey});
+    try {
+        if (transaction) {
+            transaction.update({senderWallet: wallet, recipient, amount})
+        }
+        else {
+            transaction = wallet.createTransaction({recipient, amount});
+        }
+    }
+    catch (error) {
+        return res.status(400).json({type: 'error',message: error.message})
+    }
+
+    transactionPool.setTransaction(transaction);
+
+    console.log('transactionPool: ', transactionPool);
+
+    res.json({type:'success',transaction})
 })
 
 const syncChains = () => {
